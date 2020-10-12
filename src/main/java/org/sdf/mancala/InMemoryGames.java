9bbd81da8b289399124de56a7e7fbb535c4889e8
andreoss <andreoss@sdf.org>
2020-10-12 03:14:18 +0300

    Use properties

--- src/main/java/org/sdf/mancala/InMemoryGames.java
+++ src/main/java/org/sdf/mancala/InMemoryGames.java
@@ -5,6 +5,8 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.atomic.AtomicInteger;
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import org.eclipse.microprofile.config.Config;
 
 @ApplicationScoped
 public final class InMemoryGames implements Games {
@@ -12,12 +14,22 @@ public final class InMemoryGames implements Games {
 
     private final Map<Integer, Game> games = new HashMap<>();
 
+    private final Config config;
+
+    @Inject
+    public InMemoryGames(final Config config) {
+        this.config = config;
+    }
+
     @Override
     public Game start() {
         final int id = this.count.incrementAndGet();
         final Game game = new GameOf(
             id,
-            new BoardForTwo(6, 14),
+            new BoardForTwo(
+                this.config.getValue("mancala.initialMarbles", Integer.class),
+                this.config.getValue("mancala.boardSize", Integer.class)
+            ),
             new PlayersOf(
                 new PlayerOf("first"),
                 new PlayerOf("second")
